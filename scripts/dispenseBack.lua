e4a6838dace803337c78148a8d1db4186f5c015e
Joaquin Touron <43842304+JayexDesigns@users.noreply.github.com>
2021-06-16 04:50:14 +0200

    improved dispense back

--- scripts/dispenseBack.lua
+++ scripts/dispenseBack.lua
@@ -1,11 +1,19 @@
 rednet.open("right")
 
+function SuckUp(quantity)
+    while true do
+        if turtle.suckUp(quantity) then
+            return
+        end
+    end
+end
+
 while true do
     local id, message, protocol = rednet.receive()
     if protocol == "givePotatoes" then
         print("Giving away ".. message.. " potatoes")
         for i = 1, math.floor(message / 64) do
-            turtle.suckUp()
+            turtle.suckUp(64)
             turtle.dropDown()
         end
 
